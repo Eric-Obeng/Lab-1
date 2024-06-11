7a7f1b9a63e05525dd4ff60e1c533af079252dd6
Eric-Obeng <obengeric.3958@gmail.com>
2024-06-11 16:22:43 +0000

    isPalindrome(str)

--- Weeek1.2.js
+++ Weeek1.2.js
@@ -24,5 +24,24 @@ const reverse = (str) => {
 
 /***
  * Test
- console.log(reverse("Amalitech")); 
- * */
+ console.log(reverse("Amalitech"));
+ *
+*/
+
+// isPalindrome(str): Check if a string is a palindrome
+const isPalindrome = (str) => {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+};
+// console.log(isPalindrome('aba'));
+
+
